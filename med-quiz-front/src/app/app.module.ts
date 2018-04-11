64f620b31090654e7b781cd69a2e4e116a6c7b60
kniaz <kniaz.dm@gmail.com>
2018-04-12 00:31:19 +0300

    Update app.module. App-routing.module was created.

--- med-quiz-front/src/app/app.module.ts
+++ med-quiz-front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {AdminModule} from "./admin/admin.module";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {AdminModule} from "./admin/admin.module";
   ],
   imports: [
     BrowserModule,
-    AdminModule
+    AdminModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
