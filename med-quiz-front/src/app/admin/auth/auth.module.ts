93044396a79601ca973fd6c0a9e3b0d2dc97cf68
kniaz <kniaz.dm@gmail.com>
2018-04-11 23:04:50 +0300

    Update architecture of project.

---
+++ med-quiz-front/src/app/admin/auth/auth.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from "@angular/core";
+import {LoginComponent} from "./login/login.component";
+import { RegistrationComponent } from './registration/registration.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegistrationComponent
+  ]
+})
+export class AuthModule {
+
+}
