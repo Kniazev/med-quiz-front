4d1cc64d61a16f92a0a8a40efae2e3b3f86f8c56
kniaz <kniaz.dm@gmail.com>
2018-04-11 23:11:50 +0300

    AuthModule was update.

--- med-quiz-front/src/app/admin/auth/auth.module.ts
+++ med-quiz-front/src/app/admin/auth/auth.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from "@angular/core";
 import {LoginComponent} from "./login/login.component";
 import { RegistrationComponent } from './registration/registration.component';
+import {AuthComponent} from "./auth.component";
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    AuthComponent
   ]
 })
 export class AuthModule {
