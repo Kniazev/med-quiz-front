a9de5b1acbc7138a4d1d99b98b560d48ad300755
kniaz <kniaz.dm@gmail.com>
2018-04-11 23:49:47 +0300

    Fix admin module.

--- med-quiz-front/src/app/admin/admin.module.ts
+++ med-quiz-front/src/app/admin/admin.module.ts
@@ -1,11 +1,10 @@
 import {NgModule} from "@angular/core";
 import {AuthModule} from "./auth/auth.module";
-import {CommonModule} from "@angular/common";
+
 
 @NgModule({
   imports:[
     AuthModule,
-    CommonModule
   ]
 })
 export class AdminModule{
